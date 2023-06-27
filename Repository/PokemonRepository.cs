348703f55e610e36bafc74c4919ebd709a6dc69b
Ronit Gandhi <ronitgandhi96@gmail.com>
2023-06-27 23:28:10 +0530

    Creating Repository and interface for Controller.

---
+++ Repository/PokemonRepository.cs
@@ -0,0 +1,50 @@
+using DotNetRpgPokemon.Data;
+using DotNetRpgPokemon.Interfaces;
+using DotNetRpgPokemon.Models;
+
+namespace DotNetRpgPokemon.Repository
+{
+
+	public class PokemonRepository:IPokemonRepository
+	{
+		private readonly DataContext _context;
+
+		public PokemonRepository(DataContext context)
+		{
+			_context = context;
+		}
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+
+        Pokemon IPokemonRepository.GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+           return _context.Pokemon.Where(p=>p.Name==name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(r => r.Pokemon.Id == pokeId);
+            if (review.Count() <= 0)
+                return 0;
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
+        }
+
+        public bool PokemonExists(int pokeId)
+        {
+            return _context.Pokemon.Any(p => p.Id == pokeId);                   
+        }
+
+        public ICollection<Pokemon> GetPokemon(int id)
+        {
+            return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
