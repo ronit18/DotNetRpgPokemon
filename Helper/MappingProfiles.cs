51ea0c2184b6a4d8c5ed83829d9dc2369a3f396d
Ronit Gandhi <ronitgandhi96@gmail.com>
2023-06-28 06:12:57 +0530

    Added automapper.

---
+++ Helper/MappingProfiles.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using DotNetRpgPokemon.Dto;
+using DotNetRpgPokemon.Models;
+
+namespace DotNetRpgPokemon.Helper
+{
+	public class MappingProfiles : Profile
+	{
+		public MappingProfiles()
+		{
+			CreateMap<Pokemon, PokemonDto>();
+		}
+
+	}
+}
